c529afd6b57f388cbdd85dbc199def4019c6903b
Wasiur Rahman <wrahman0@gmail.com>
2014-06-19 00:11:14 -0400

    Keep player on screen

--- Example 6 - Images/scripts/app.js
+++ Example 6 - Images/scripts/app.js
@@ -220,6 +220,30 @@ function checkCollisions(){
 			}
 		}
 
+		if (boxCollides(pos, size, player.pos, player.sprite.size)){
+			gameOver();
+		}
 	}
 }
+
+
+function checkPlayerBounds(){
+	
+	if (player.pos[0] < 0){
+		player.pos[0] = 0;
+	}
+	else if (player.pos[0] > canvas.width - player.sprite.size[0]){
+		player.pos[0] = canvas.width - player.sprite.size[0];
+	}
+
+	if (player.pos[1] < 0){
+		player.pos[1] = 0;
+	}
+	else if (player.pos[1] > canvas.height - player.sprite.size[1]){
+		player.pos[1] = canvas.height - player.sprite.size[1];
+	}
+}
+
+
+
 
